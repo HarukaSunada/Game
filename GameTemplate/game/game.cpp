39181dfff9df1dd637f17fdca5d2fbd98bbe3e96
Sunada Haruka <a>
2017-07-11 16:46:24 +0900

    カメラ仮

---
+++ GameTemplate/game/game.cpp
@@ -0,0 +1,46 @@
+/*!
+ * @brief	ゲーム
+ */
+#include "stdafx.h"
+#include "game.h"
+
+
+/*!
+ * @brief	コンストラクタ。
+ */
+Game::Game()
+{
+}
+/*!
+ * @brief	デストラクタ。
+ */
+Game::~Game()
+{
+}
+/*!
+ * @brief	ゲームが起動してから一度だけ呼ばれる関数。
+ */
+void Game::Start()
+{
+	//カメラを初期化
+	camera.Init();
+	camera.SetEyePt(D3DXVECTOR3(0.0f, 4.0f, 10.0f));
+	camera.SetLookatPt(D3DXVECTOR3(0.0f, 2.5f, 0.0f));
+	camera.SetFar(1000.0f);
+	camera.Update();
+	toCameraPos = camera.GetEyePt() - camera.GetLookatPt();
+}
+/*!
+ * @brief	更新。
+ */
+void Game::Update()
+{
+	//カメラ更新
+	camera.Update();
+}
+/*!
+ * @brief	描画。
+ */
+void Game::Render()
+{
+}
